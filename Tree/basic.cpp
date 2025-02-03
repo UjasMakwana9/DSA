2af01ae077fa52021bc5fc20ff3b48e4ea1a579a
Ujas Makwana <makwanaujas99@gmail.com>
2025-02-03 23:53:57 +0530

    Add implementations for binary tree height and level order traversal in C++

--- Tree/basic.cpp
+++ Tree/basic.cpp
@@ -2,7 +2,7 @@
 
 
 using namespace std;
-
+// Data Structure
 class Node{
     public:
     int data;
@@ -14,6 +14,7 @@ class Node{
         left = right = NULL;
     }
 };
+// Traversal
 // Time Complexity: O(n)
 // Space Complexity: O(n)
 void inOrder(Node * root){
@@ -41,6 +42,71 @@ void postOrder(Node * root){
     preOrder(root->right);
     cout<<root->data<<" ";
 }
+
+// Height of the Tree
+int heightofBinaryTree(Node * root){
+    if(root == NULL){
+        return 0;
+    }
+    return max(heightofBinaryTree(root->left), heightofBinaryTree(root->right)) + 1;
+}
+
+// Print nodes at a current level
+void printCurrentLevel(Node* root, int level) {
+    if (root == nullptr)
+        return;
+    if (level == 1)
+        cout << root->data << " ";
+    else if (level > 1) {
+        printCurrentLevel(root->left, level - 1);
+        printCurrentLevel(root->right, level - 1);
+    }
+}
+
+// Breadth First Search
+// Time Complexity O(n^2)
+// Space Complexity O(n)==>Height of the tree (max)
+void printLevelOrder(Node* root) {
+    int h = heightofBinaryTree(root);
+    for (int i = 1; i <= h; i++)
+        printCurrentLevel(root, i);
+}
+
+
+
+
+// Optimal Solution
+// Time Complexity : O(n)
+// Space Complexity : O(n)
+ vector<vector<int>> levelOrder(Node *root) {
+        // code here
+        vector<vector<int>> v;
+        if(root==NULL)
+            return v;
+            
+        queue<Node *>q;
+        q.push(root);
+        v.push_back({root->data});
+        while(q.size()>0)
+        {
+            Node * curr = q.front();
+            q.pop();
+            vector<int>temp;
+            if(curr->left!=NULL)
+            {
+                temp.push_back(curr->left->data);
+                q.push(curr->left);
+            }
+            if(curr->right!=NULL)
+            {
+                temp.push_back(curr->right->data);
+                q.push(curr->right);
+            }
+            v.push_back(temp);
+        }
+        
+        return v;
+    }
 int main()
 {
     Node * root = new Node(10);
@@ -55,5 +121,10 @@ int main()
     preOrder(root);
     cout<<endl<<"Postorder Traversal: ";
     postOrder(root);
+    cout<<endl<<"Height of the Tree: "<<heightofBinaryTree(root);
+    cout<<endl<<"Level Order Traversal: ";
+    printLevelOrder(root);
+
+    
 return 0;
 }
